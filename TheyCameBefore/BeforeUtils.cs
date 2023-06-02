2355c0b397825232c3dbfe14f191df1ba88fb7c6
HenriqueMonteiro <Henriquepupet@gmail.com>
2023-06-02 13:38:54 +0100

    Add new GetTheOnesBefore static

--- TheyCameBefore/BeforeUtils.cs
+++ TheyCameBefore/BeforeUtils.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TheyCameBefore
+public static class BeforeUtils
 {
-    public class BeforeUtils
+    public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items, T target) where T : struct, IComparable<T>
     {
-        
+        bool foundTarget = false;
+        foreach (T item in items)
+        {
+            if (item.CompareTo(target) == 0)
+            {
+                foundTarget = true;
+                break;
+            }
+
+            yield return item;
+        }
+
+        if (!foundTarget)
+        {
+            throw new ArgumentException("O item alvo não foi encontrado no enumerável.");
+        }
     }
+
 }
